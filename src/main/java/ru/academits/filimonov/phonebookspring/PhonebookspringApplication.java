0105de67a891e55396f26c2ec730f859755b19b2
vfilim <rashovlad@gmail.com>
2021-11-06 02:06:07 +0700

    отформатировал

--- src/main/java/ru/academits/filimonov/phonebookspring/PhonebookspringApplication.java
+++ src/main/java/ru/academits/filimonov/phonebookspring/PhonebookspringApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PhonebookspringApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PhonebookspringApplication.class, args);
     }
